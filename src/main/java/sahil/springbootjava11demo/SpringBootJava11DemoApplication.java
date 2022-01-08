bd8f6de84e1da3e911e1262c909cb82a1d5e2cb4
innovativesahil <innovativesahil95@gmail.com>
2022-01-08 22:00:35 +0530

    Initial Commit
    - Spring Boot Web
    - Spring Boot JPA with H2
    - Spring Boot Kafka
    - Lombok
    - Devtools and Actuator

---
+++ src/main/java/sahil/springbootjava11demo/SpringBootJava11DemoApplication.java
@@ -0,0 +1,13 @@
+package sahil.springbootjava11demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJava11DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJava11DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sahil/springbootjava11demo/SpringBootJava11DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sahil.springbootjava11demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJava11DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
